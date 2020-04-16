26505ea27af13d2dd76f062003ce29cd5a5cae1b
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-16 15:22:40 -0300

    Flunt e domain notifications

--- Todo.Domain/Commands/CreateTodoCommand.cs
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -1,9 +1,11 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand : ICommand
+    public class CreateTodoCommand : Notifiable, ICommand
     {
         public CreateTodoCommand() { }
         public CreateTodoCommand(string title, string user, DateTime date)
@@ -18,7 +20,12 @@ namespace Todo.Domain.Commands
 
         public void Validate()
         {
-            throw new System.NotImplementedException();
+            AddNotifications(
+                new Contract()
+                        .Requires()
+                        .HasMinLen(Title, 3, "Title", "Por favor, descreva melhor essa tarefa!")
+                        .HasMinLen(User, 6, "User", "Usuário inválido")
+            );
         }
     }
 }
