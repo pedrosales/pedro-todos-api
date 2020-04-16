2711a3966ab41702d71eacc2a06c6c589dbd4ded
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-16 16:10:37 -0300

    Commands e Generic command result

---
+++ Todo.Domain/Commands/MarkTodoAsUndoneCommand.cs
@@ -0,0 +1,28 @@
+using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarkTodoAsUndone : Notifiable, ICommand
+    {
+        public MarkTodoAsUndone() { }
+        public MarkTodoAsUndone(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                .Requires()
+                .HasMinLen(User, 6, "User", "Usuário iválido")
+            );
+        }
+    }
+}
