777ca21eff8fd3808bbaa395e1c2b258c689fce1
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-16 21:32:20 -0300

    DI do data context

--- Todo.Domain.Api/Startup.cs
+++ Todo.Domain.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Todo.Domain.Infra.Contexts;
 
 namespace Todo.Domain.Api
 {
@@ -26,6 +21,8 @@ namespace Todo.Domain.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
